6c523364ae9b25f9dbe4edd89de601ea9cfa9762
shinichiost <81664572+shinichiost@users.noreply.github.com>
2022-05-07 08:54:46 +0700

    abc

---
+++ src/main/java/tacos/Th2N20Application.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Th2N20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Th2N20Application.class, args);
+	}
+
+}
---
+++ src/test/java/tacos/Th2N20ApplicationTests.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Th2N20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
